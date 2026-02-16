92942f3a919d875f8172955ac7f8d010a4c52793
klayyy122 <maksa0982@gmail.com>
2026-02-16 19:52:15 +0300

    update

--- cpp/normal.cpp
+++ cpp/normal.cpp
@@ -12,104 +12,253 @@
 #include <chrono>
 #include <vector>
 #include <atomic>
+#include<future>
+#include<queue>
+#include<condition_variable>
+#include<shared_mutex>
+#include<functional>
 
-
-std::mutex mtx;
+std::mutex result_mtx;
+std::shared_mutex log_mtx;
 namespace fs = std::filesystem;
 
 
-void process(const std::vector<fs::path> files, const std::string& output_dir, std::atomic<int>& count_of_success, std::atomic<int>& count_of_failed) {
-    for(const auto& file_path : files) {
-       
-        int width, height, channels;
-        unsigned char* img = stbi_load(file_path.string().c_str(), &width, &height, &channels, 0);
-        
-        if(!img) {
-            ++count_of_failed;
-            continue;
-        }
-        
-      
-        int total_pixels = width * height * channels;
-        for(int i = 0; i < total_pixels; i++) {
-            img[i] = 255 - img[i];
-        }
+bool is_digit(char* cstr){
 
-        std::string filename = file_path.stem().string();
-        std::string output_path = output_dir + "/inverted_" + filename + ".png";
+    std::string str(cstr);
+    return !str.empty() && std::all_of(str.begin(), str.end(), ::isdigit);
+}
+template<typename T>
+class BlockingQueue {
+    std::queue<T> queue_;
+    std::mutex mtx;
+    std::condition_variable cv;
+    std::atomic<bool> is_finished = false;
 
+public:
+    void push(const T& item){
         std::unique_lock<std::mutex> lock(mtx);
-
-        std::cout << "Proccessed: " << output_path << std::endl;
-
+        queue_.push(item);
         lock.unlock();
-        if(!stbi_write_png(output_path.c_str(), width, height, channels, img, width * channels)) {
-            ++count_of_failed;
-        } else {
-            ++count_of_success;
+        cv.notify_one();
+    }
+    bool pop(T& item){
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this]
+                { return !queue_.empty() || is_finished; });
+
+        if(queue_.empty() && is_finished){
+            return false;
         }
-        
-        stbi_image_free(img);
+
+        item = queue_.front();
+        queue_.pop();
+        lock.unlock();
+        return true;
+    }
+
+    void set_finished() {
+        is_finished = true;
+        cv.notify_all();
+    }
+
+    bool is_empty(){
+        std::unique_lock<std::mutex> lock(mtx);
+        return queue_.empty();
+    }
+};
+
+struct ImageTask {
+    fs::path input_path;
+    std::string output_path;
+};
+
+struct ImageResult {
+    std::string input_file;
+    std::string output_file;
+    bool success;
+};
+
+void producer(BlockingQueue<ImageTask>& task_queue,
+             const std::vector<fs::path>& image_files,
+             const std::string& output_dir,
+             std::atomic<bool>& complete){
+    
+    for(const auto& file : image_files){
+
+        std::string filename = file.stem().string();
+        std::string output_path = output_dir + "/inverted_" + filename + file.extension().string();
+
+        ImageTask task{file, output_path};
+
+        task_queue.push(task);
+        std::unique_lock<std::shared_mutex> lock(log_mtx);
+        std::cout << "Producer: Added task for " << file.filename() << std::endl;
+    }
+
+    complete.store(true);
+    task_queue.set_finished();
+
+              
+    {
+        std::unique_lock<std::shared_mutex> lock(log_mtx);
+        std::cout << "Producer: All tasks added. Total: " << image_files.size() << std::endl;
     }
 }
 
-int main(int argc, char** argv) {
-    int NUM_THREADS = 4;
-    if(argc > 1) {
-        NUM_THREADS = std::atoi(argv[1]);
+void consumer(BlockingQueue<ImageTask>& task_queue, std::vector<ImageResult>& results, std::atomic<int>& processed_count, int consumer_id ){
+
+    ImageTask task;
+
+    while(task_queue.pop(task)){
+
+        std::unique_lock<std::shared_mutex> lock(log_mtx);
+        std::cout << "Consumer " << consumer_id << ": Processing " 
+                     << task.input_path.filename() << std::endl;
+        lock.unlock();
+        
+        int width, height, channels;
+        unsigned char *img = stbi_load(task.input_path.string().c_str(), &width, &height, &channels, 0);
+
+        ImageResult result;
+        result.input_file = task.input_path.string();
+        result.output_file = task.output_path;
+
+        if(!img){
+            result.success = false;
+
+            lock.lock();
+            std::cout << "Consumer " << consumer_id << ": Failed to load "
+                      << task.input_path.filename() << std::endl;
+            lock.unlock();
+        }else{
+            int total_pixeles = height * width * channels;
+            for (int i = 0; i < total_pixeles; ++i){
+                img[i] = 255 - img[i];
+            }
+
+            if(!stbi_write_png(task.output_path.c_str(), width, height, channels, img, width * channels)) {
+                result.success = false;
+
+            }else{
+                result.success = true;
+
+                lock.lock();
+                std::cout << "Consumer " << consumer_id << ": Completed "
+                          << task.input_path.filename() << std::endl;
+            }
+        }
+
+        stbi_image_free(img);
+
+        std::unique_lock<std::mutex> lock1(result_mtx);
+        results.push_back(result);
+        lock1.unlock();
+        processed_count++;
     }
+    std::unique_lock<std::shared_mutex> lock(log_mtx);
+    std::cout << "Consumer " << consumer_id << ": finished work" << std::endl;
+}
 
-    std::string input_dir = "./dataset";
-    std::string output_dir = "./results/images";
+int main(int argc, char** argv){
+    int num_consumers = 4;
 
-    if(!fs::exists(input_dir)) {
-        std::cerr << "Error: Directory '" << input_dir << "' doesn't exist!\n";
+    if(argc == 2){
+        if(is_digit(argv[1])){
+
+            num_consumers = std::atoi(argv[1]);
+        }else{
+
+            std::cerr << "Enter correct integer value(<= " << std::thread::hardware_concurrency() << ")" << std::endl;
+            ;
+            return 2;
+        }
+    }else{
+        std::cerr << "Usage: <threads_num>\n";
         return 1;
     }
 
-    if(!fs::exists(output_dir)) {
-        fs::create_directory(output_dir);
+    std::string input_dir = "../dataset";
+    std::string output_dir = "../results";
+    
+    if(!fs::exists(input_dir)){
+        std::cerr << "Directory " << input_dir << " does not exist!!!\n";
+        return -1;
     }
 
-    std::set<std::string> extensions = {".png", ".jpeg", ".jpg"};
+    if(!fs::exists(output_dir)){
+        fs::create_directories(output_dir);
+    }
 
-    //put all files in one vector
+    std::set<std::string> extensions = {".png", ".jpeg", ".jpg", ".bmp", ".tiff"};
     std::vector<fs::path> image_files;
 
-    for(const auto& file : fs::directory_iterator(input_dir)) {
-        if(file.is_regular_file()) {
+    for(const auto& file : fs::directory_iterator(input_dir)){
+        if(file.is_regular_file()){
             std::string ext = file.path().extension().string();
-            
-             //check the correctness of extension
-            bool supported = find(extensions.begin(), extensions.end(), ext) != extensions.end();
-            if(!supported) continue;
+            std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
 
-            image_files.push_back(file.path());
+            if(extensions.find(ext) != extensions.end()){
+                image_files.push_back(file.path());
+            }
         }
     }
+    if(image_files.empty()){
+        std::cerr << "No image files in directory" << std::endl;
+    }
 
-    if(image_files.empty()) {
-        std::cerr << "Error: No image files found in dataset!" << std::endl;
-        return 1;
+    std::cout << "Found " << image_files.size() << " images to process" << std::endl;
+    std::cout << "Using " << num_consumers << " consumer threads" << std::endl;
+
+    BlockingQueue<ImageTask> task_queue;
+    std::vector<ImageResult> results;
+
+    std::atomic<int> processed_count(0);
+    std::atomic<bool> production_complete(false);
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+    
+    std::thread producer_thread(producer, std::ref(task_queue), 
+                                std::ref(image_files), 
+                                std::ref(output_dir),
+                                std::ref(production_complete));
+    
+  
+    std::vector<std::thread> consumer_threads;
+    for (int i = 0; i < num_consumers; i++){
+        consumer_threads.emplace_back(consumer, std::ref(task_queue),
+                                      std::ref(results), 
+                                      std::ref(processed_count), i);
     }
     
-    std::vector<std::vector<fs::path>> pieces(NUM_THREADS);
-    for(size_t i = 0; i < image_files.size(); i++) {
-        pieces[i % NUM_THREADS].push_back(image_files[i]);
-    }
 
-    std::atomic<int> success_count(0);
-    std::atomic<int> fail_count(0);
-    std::vector<std::thread> threads;
-
-
-    for(int i = 0; i < NUM_THREADS; i++) {
-        threads.emplace_back(process, pieces[i], output_dir, std::ref(success_count), std::ref(fail_count));             
-    }
-
-    for(auto& thread : threads) {
+    producer_thread.join();
+    
+    for (auto& thread : consumer_threads) {
         thread.join();
     }
-
+   
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>
+                   (end_time - start_time);
+    int success_count = 0;
+    int fail_count = 0;
+    
+    for (const auto& result : results){
+        if(result.success) {
+            success_count++;
+        }else{
+            fail_count++;
+            std::cout << "Failed: " << result.input_file 
+                     << std::endl;
+        }
+    }
+    
+    std::cout << "\n=== Processing Complete ===" << std::endl;
+    std::cout << "Total images: " << image_files.size() << std::endl;
+    std::cout << "Successfully processed: " << success_count << std::endl;
+    std::cout << "Failed: " << fail_count << std::endl;
+    std::cout << "Processing time: " << duration.count() << " stconds" << std::endl;
+    
     return 0;
-}   
+}
